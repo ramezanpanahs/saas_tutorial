d703b1633e18ad50cdd31e86c13a1f4a7bcb63e9
saas <saas@saas.com>
2016-03-07 09:32:37 +0100

    Added publisher

--- src/publisher.cpp
+++ src/publisher.cpp
@@ -1,13 +1,19 @@
 #include <ros/ros.h>
+#include <std_msgs/String.h>
 
 int main(int argc, char** argv){
 
     ros::init(argc,argv,"publisher");
     ros::NodeHandle nh;
+    ros::Publisher our_pub = nh.advertise<std_msgs::String>("chatter", 10);
+
     ros::Rate rate(2); //Hz
 
     while (nh.ok()){
+        std_msgs::String our_msg;
+        our_msg.data = "Hello"
         ROS_INFO("hi Sam!");
+        our_pub.publish(our_msg);
         rate.sleep();
     }
 
